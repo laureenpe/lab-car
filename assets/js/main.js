8f610146f1218ce4562908961f981bd9dbb8dbca
“Laura <laureen.pelaez.isaza@gmail.com>
2017-06-21 23:17:56 -0400

    updated footer and navbar responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
+/*var map;//var global para poderla llamar 
+
 function initMap() {
     var directionsService = new google.maps.DirectionsService;
     var directionsDisplay = new google.maps.DirectionsRenderer;
-    var map = new google.maps.Map(document.getElementById("map"), {
+     map = new google.maps.Map(document.getElementById("map"), {
         zoom: 5,
         center: { lat: -9.1191427, lng: -77.0349046 },
         mapTypeControl: false,
@@ -16,42 +18,13 @@ function initMap() {
 
     directionsDisplay.setMap(map);//funcion para buscar la ruta en el mapa
     var origenAutoComp = (document.getElementById('start'));
-          var autocompletar = new google.maps.places.Autocomplete(origenAutoComp);
-          autocompletar.bindTo('bounds', map);
+    var autocompletar = new google.maps.places.Autocomplete(origenAutoComp);
+    autocompletar.bindTo('bounds', map);
 
-         var destinoAutoComp = (document.getElementById('end'));
-          var autocompletar = new google.maps.places.Autocomplete(destinoAutoComp);
-          autocompletar.bindTo('bounds', map);
+    var destinoAutoComp = (document.getElementById('end'));
+    var autocompletar = new google.maps.places.Autocomplete(destinoAutoComp);
+    autocompletar.bindTo('bounds', map);
 
-
-
-    function buscar() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
-        }
-    }
-
-    document.getElementById("encuentrame").addEventListener("click", buscar);
-    var latitud, longitud;
-
-    var funcionExito = function (posicion) {
-        latitud = posicion.coords.latitude;
-        longitud = posicion.coords.longitude;
-
-        var miUbicacion = new google.maps.Marker({
-            position: { lat: latitud, lng: longitud },
-            animation: google.maps.Animation.DROP,
-            icon: 'https://cdn4.iconfinder.com/data/icons/48x48-free-object-icons/48/Bike.png',//poner el icono de bicicleta :https://developers.google.com/maps/documentation/javascript/custom-markers
-            map: map
-        });
-        map.setZoom(17);
-        map.setCenter({ lat: latitud, lng: longitud });
-
-    }
-
-    var funcionError = function (error) {
-        alert("Tenemos un problema con encontrar tu ubicacion");
-    }
     //DIRECTIONS
     var onClick = function () { //llama la funcion creada para mostrar la direcciòn
         calculateAndDisplayRoute(directionsService, directionsDisplay);
@@ -75,3 +48,30 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
     });
 }
 
+buscar();
+
+function buscar() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
+    }
+}
+
+function funcionExito(posicion) {
+    var latitud, longitud;
+    latitud = posicion.coords.latitude;
+    longitud = posicion.coords.longitude;
+
+    var miUbicacion = new google.maps.Marker({
+        position: { lat: latitud, lng: longitud },
+        animation: google.maps.Animation.DROP,
+        icon: 'https://cdn4.iconfinder.com/data/icons/48x48-free-object-icons/48/Bike.png',//poner el icono de bicicleta :https://developers.google.com/maps/documentation/javascript/custom-markers
+        map: map
+    });
+    map.setZoom(17);
+    map.setCenter({ lat: latitud, lng: longitud });
+}
+
+function funcionError(error) {
+    alert("Tenemos un problema con encontrar tu ubicacion");
+}
+/*
